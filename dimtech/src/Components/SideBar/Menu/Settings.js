cf8a4ad78b26ad5d560a1354e2eb68fca4c8eee3
Chin Valery <chinvalery@gmail.com>
2021-01-04 12:32:52 +0100

    create space for settings component and adding some padding in dashboard normaly i have to switch branch but i\'m the only one of this project ˆˆ

--- dimtech/src/Components/SideBar/Menu/Settings.js
+++ dimtech/src/Components/SideBar/Menu/Settings.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Sidebar from '../Sidebar'
+import './style/Settings.css'
 
-export default function Portfolio() {
+export default function Settings() {
 
   return (
-    <div className="Container-setting">
+    <div className="Main-container">
       <Sidebar />
+      <div className="Container-settings">
+        <div className="Container-setelement">
+          <div className="Profile-info">
+
+          </div>
+          <div className="offers">
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
