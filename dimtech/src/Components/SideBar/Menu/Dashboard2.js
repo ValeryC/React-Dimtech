91c5f3f467859f3b62352a999feb334cc8eb02d3
Chin Valery <chinvalery@gmail.com>
2021-01-06 18:04:48 +0100

    try to push info

--- dimtech/src/Components/SideBar/Menu/Dashboard2.js
+++ dimtech/src/Components/SideBar/Menu/Dashboard2.js
@@ -1,15 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import DashboardBlock from './DashboardComp/DashboardBlock'
 import "./style/Dashboard2.css"
 import ArrowLeft from '../../ArrowLeft'
 import ArrowRight from '../../ArrowRight'
+import axios from 'axios'
 
 export default function Dashboard2() {
 
+  // useEffect(() => {
+  // })
+
+
+  // const url = "https://coronavirusapi-france.now.sh/FranceLiveGlobalData"
+
+  const [data, setData] = useState({ hits: [] });
+
   useEffect(() => {
     document.title = 'Dashboard2'
-  })
+    const fetchData = async () => {
+      const result = await axios(
+        "https://coronavirusapi-france.now.sh/FranceLiveGlobalData",
+      );
+      console.log(result.data)
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
+
 
   return (
     <div className="Main-container">
@@ -17,8 +36,11 @@ export default function Dashboard2() {
       <div className="Container-dashboard">
         <DashboardBlock />
         <div className="Container-dashelement2">
-          <div className="block-dash">
+          <div className="block-dash" key={data}>{data.deces}
             Type
+            <ul>
+
+            </ul>
           </div>
           <div className="block-dash" style={{ backgroundColor: '#707070', color: 'white' }}>
             of
